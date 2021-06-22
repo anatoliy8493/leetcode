43f7a7c98f53ad8b330a623ab9e812a897e0fc04
Anatolii Kurochkin <anatoliy841993@gmail.com>
2021-06-22 10:21:23 -0700

    Time: 80 ms (75.18%), Space: 40.6 MB (16.21%) - LeetHub

---
+++ two-sum/two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  const map = {};
+  
+  for (let i = 0; i < nums.length; i += 1) {
+    if (map[nums[i]] !== undefined) return [map[nums[i]], i];
+
+    map[target - nums[i]] = i;
+  }
+};
