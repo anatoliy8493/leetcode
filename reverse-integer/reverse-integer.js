f964100992cfdd544482d291063bc2b0db7075c6
Anatolii Kurochkin <anatoliy841993@gmail.com>
2021-06-23 20:34:20 -0700

    Time: 92 ms (83.94%), Space: 40.5 MB (29.42%) - LeetHub

--- reverse-integer/reverse-integer.js
+++ reverse-integer/reverse-integer.js
@@ -3,9 +3,6 @@
  * @return {number}
  */
 var reverse = function(x) {
-  // 123 % 100 3
-  // 120 % 100 2
-  // 100 % 100 1
   const result = +String(Math.abs(x)).split('').reverse().join('');
   
   if (result <= -(2 ** 31) || result >= 2 ** 31 - 1) return 0;
