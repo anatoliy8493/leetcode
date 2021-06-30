ced4efb534509c477d05cd3e77b520e8490dcb8c
Anatolii Kurochkin <anatoliy841993@gmail.com>
2021-06-30 11:10:13 -0700

    Time: 92 ms (78.25%), Space: 41.3 MB (53.05%) - LeetHub

--- valid-anagram/valid-anagram.js
+++ valid-anagram/valid-anagram.js
@@ -4,27 +4,16 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  if (s.length !== t.length) return false;
+  if (s.length !== t.length) return false
   
-  const sMap = {};
-  const tMap = {};
+  const sMap = new Map();
+  const tMap = new Map();
+  
+  for (let i = 0; i < s.length; i += 1) sMap.set(s[i], sMap.has(s[i]) ? sMap.get(s[i]) + 1 : 1);
+  for (let i = 0; i < t.length; i += 1) tMap.set(t[i], tMap.has(t[i]) ? tMap.get(t[i]) + 1 : 1);
   
   for (let i = 0; i < s.length; i += 1) {
-    if (sMap[s[i]]) {
-      sMap[s[i]] += 1;
-    } else {
-      sMap[s[i]] = 1;
-    }
-    
-    if (tMap[t[i]]) {
-      tMap[t[i]] += 1;
-    } else {
-      tMap[t[i]] = 1;
-    }
-  }
-  
-  for (key in sMap) {
-    if (tMap[key] !== sMap[key]) return false;
+    if (sMap.get(s[i]) !== tMap.get(s[i])) return false;
   }
   
   return true;
