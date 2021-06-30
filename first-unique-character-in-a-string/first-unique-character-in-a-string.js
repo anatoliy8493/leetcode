00013ab16770b1232401462fb84b50433f86b3be
Anatolii Kurochkin <anatoliy841993@gmail.com>
2021-06-29 21:24:00 -0700

    Time: 120 ms (52.29%), Space: 41.2 MB (99.51%) - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -3,18 +3,14 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-  const hashMap = new Map();
+  const map = new Map();
   
   for (let i = 0; i < s.length; i += 1) {
-    if (hashMap.has(s[i])) {
-      hashMap.set(s[i], hashMap.get(s[i]) + 1);
-    } else {
-      hashMap.set(s[i], 1);
-    }
+    map.set(s[i], map.has(s[i]) ? map.get(s[i]) + 1 : 1);
   }
   
   for (let i = 0; i < s.length; i += 1) {
-    if (hashMap.get(s[i]) === 1) return i;
+    if (map.get(s[i]) === 1) return i;
   }
   
   return -1;
